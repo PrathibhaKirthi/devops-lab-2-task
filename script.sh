e88b54e61868232d303f4364fbad00f1b27060ad
PrathibhaKirthi <prathibhakirthi@gmail.com>
2026-01-30 07:55:19 +0000

    Delete folders at script start

--- script.sh
+++ script.sh
@@ -1,14 +1,15 @@
-
- 	#!/bin/bash
-
-mkdir fol_1 fol_2
-
-
-
 #!/bin/bash
 
+# Delete folders if they already exist
+rm -rf fol_1 fol_2
+
+# Create folders
 mkdir fol_1 fol_2
 
+# Create files in fol_1
 touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
+
+# Create files in fol_2
 touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+
 
